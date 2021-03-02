1575a344406cdfa2b29efedb3a8750f2f0d780e5
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2021-03-01 23:45:13 -0500

    redone

--- app.js
+++ app.js
@@ -15,47 +15,50 @@
 //     alert('you clicked the pos');
 // });
 
-
-
-//from w3 schools
-// Get the modal
-var dplanModal = document.getElementById("dplanModal");
-var baconModal = document.getElementById("baconModal");
-
-// Get the button that opens the modal
-var dplanBtn = document.getElementById("dplanBtn");
-var baconBtn = document.getElementById("baconBtn");
-
-// Get the <span> element that closes the modal
-var dplanSpan = document.getElementsByClassName("closeDplan")[0];
-var baconSpan = document.getElementsByClassName("closeBacon")[0];
-
-// When the user clicks on the button, open the modal
-dplanBtn.onclick = function() {
-  dplanModal.style.display = "block";
+var portfolio = document.getElementById("labeled");
+portfolio.onclick = function() {
+  alert("yolo!!!!");
 }
 
-baconBtn.onclick = function() {
-   baconModal.style.display = "block";
-  }
+// //from w3 schools
+// // Get the modal
+// var dplanModal = document.getElementById("dplanModal");
+// var baconModal = document.getElementById("baconModal");
 
-// When the user clicks on <span> (x), close the modal
-dplanSpan.onclick = function() {
-  dplanModal.style.display = "none";
-}
-baconSpan.onclick = function() {
-    baconModal.style.display = "none";
-  }
+// // Get the button that opens the modal
+// var dplanBtn = document.getElementById("dplanBtn");
+// var baconBtn = document.getElementById("baconBtn");
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == dplanModal) {
-    dplanModal.style.display = "none";
-  }
-}
+// // Get the <span> element that closes the modal
+// var dplanSpan = document.getElementsByClassName("closeDplan")[0];
+// var baconSpan = document.getElementsByClassName("closeBacon")[0];
 
-window.onclick = function(event) {
-    if (event.target == baconModal) {
-      baconModal.style.display = "none";
-    }
-  }
+// // When the user clicks on the button, open the modal
+// dplanBtn.onclick = function() {
+//   dplanModal.style.display = "block";
+// }
+
+// baconBtn.onclick = function() {
+//    baconModal.style.display = "block";
+//   }
+
+// // When the user clicks on <span> (x), close the modal
+// dplanSpan.onclick = function() {
+//   dplanModal.style.display = "none";
+// }
+// baconSpan.onclick = function() {
+//     baconModal.style.display = "none";
+//   }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == dplanModal) {
+//     dplanModal.style.display = "none";
+//   }
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == baconModal) {
+//       baconModal.style.display = "none";
+//     }
+//   }

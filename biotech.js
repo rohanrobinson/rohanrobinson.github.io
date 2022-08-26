fc4db22f4d6d1971dd747116b98c63eaf029c6a1
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2022-08-25 21:42:36 -0500

    asdf

--- biotech.js
+++ biotech.js
@@ -1,9 +1,9 @@
 
-// navigate to sign up / survey flow if user is new and ready to go
-document.getElementById("new").addEventListener("click", function(){alert("Shortly you'll be signing up for Engagement")});
+// navigate to sign up / survey flow if user is new and ready to go //
+document.getElementById("new").addEventListener("click", function(){alert("Shortly you'll be signing up for EngageIn Biotech")});
 
-// navigate to sign in / welcome back flow if user is current 
+// navigate to sign in / welcome back flow if user is current //
 document.getElementById("familiar").addEventListener("click", function(){alert("Welcome back! Now where did we leave off last?")});
 
-// navigate to FAQ / abous us flow if user is new and not sure about goin'
+// navigate to FAQ / abous us flow if user is new and not sure about goin' //
 document.getElementById("faq").addEventListener("click", function(){alert("Let's get your questions answered!")});
